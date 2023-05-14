ab325bad31989bc01829d33e049b03960eba2797
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2023-05-14 17:40:34 -0300

    feat:ConfigJavaScript

---
+++ src/main.js
@@ -0,0 +1,28 @@
+let input = document.querySelector('#input');
+let counter = document.querySelector('.counter');
+let btnMode = document.querySelector('.mode');
+let mode = 'Char';
+let count = 0;
+input.addEventListener('input', () => {
+    if(mode === 'Char'){
+        count = input.value.length;
+        counter.textContent = `${count} Caractere(s)`;
+    } else {
+        let word = input.value.trim().split(/\s+/);
+        count = word.length;
+        counter.textContent = `${count} Palavra(s)`
+    };
+})
+
+btnMode.addEventListener('click', () => {
+    if(mode === 'Char'){
+        mode = 'Word';
+        btnMode.textContent = 'Contar Caracteres'
+        input.focus();
+    }else {
+        mode = 'Char';
+        btnMode.textContent = 'Contar Palavras'
+        input.focus();
+    }
+    input.dispatchEvent(new Event('input'));
+});
